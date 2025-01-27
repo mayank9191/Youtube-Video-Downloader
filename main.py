e3504753079968c7888f488a81fea49d9036781d
Mayank Kulahara <mayankkulahara@gmail.com>
2025-01-27 22:18:21 +0530

    inside a function set

--- main.py
+++ main.py
@@ -4,20 +4,26 @@ import os
 # Video URL
 url = input("Enter the video URL you want to download ->")
 
-# Create downloads folder if it doesn't exist
-os.makedirs('downloads', exist_ok=True)
 
-# yt-dlp options
-options = {
-    'format': 'bestvideo[ext=mp4]+bestaudio[ext=mp4]',
-    'merge_output_format': 'mp4',
-    'outtmpl': 'downloads/%(title)s.%(ext)s'
-}
+def download_yt_video(url):
 
-try:
-    # Download the video
-    with YoutubeDL(options) as ydl:
-        ydl.download([url])
-    print("Download completed successfully!")
-except Exception as e:
-    print(f"An error occurred: {e}")
+    # Create downloads folder if it doesn't exist
+    os.makedirs('downloads', exist_ok=True)
+
+    # yt-dlp options
+    options = {
+        'format': 'bestvideo[ext=mp4]+bestaudio[ext=mp4]',
+        'merge_output_format': 'mp4',
+        'outtmpl': 'downloads/%(title)s.%(ext)s'
+    }
+
+    try:
+        # Download the video
+        with YoutubeDL(options) as ydl:
+            ydl.download([url])
+        print("Download completed successfully!")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+download_yt_video(url)
