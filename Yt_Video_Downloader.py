7898cc62b776332777272dc327577da46c1b1b01
Mayank Kulahara <mayankkulahara@gmail.com>
2025-01-30 22:36:23 +0530

    new feature add

--- Yt_Video_Downloader.py
+++ Yt_Video_Downloader.py
@@ -37,7 +37,7 @@ def download_yt_audio_only(url):
     # yt-dlp options
     options = {
         'format': 'bestaudio[ext=mp4]',
-        'outtmpl': 'static/videos/%(title)s.%(ext)s'
+        'outtmpl': 'static/audio/%(title)s.%(ext)s'
     }
 
     try:
@@ -66,3 +66,67 @@ def download_yt_video_only(url):
         print("Download completed successfully!")
     except Exception as e:
         print(f"An error occurred: {e}")
+
+
+def download_transcript(url, lang='en', auto_sub=True):
+    options = {
+        'writesubtitles': True,  # Enable subtitle download
+        'subtitleslangs': [lang],  # Specify subtitle language
+        'skip_download': True,  # Don't download the video
+        'quiet': True,  # Suppress extra logs
+        'outtmpl': 'static/transcript/%(title)s.%(ext)s'  # Set output folder
+    }
+
+    # Choose auto-generated or manually uploaded subtitles
+    if auto_sub:
+        # Download auto-generated subtitles
+        options['writeautomaticsub'] = True
+    else:
+        # Download manually uploaded subtitles
+        options['writeautomaticsub'] = False
+
+    with YoutubeDL(options) as ydl:
+        ydl.download([url])
+
+
+def download_thumbnail(url):
+
+    options = {
+        'writethumbnail': True,  # Download thumbnail
+        'skip_download': True,  # Don't download the video
+        # Save the thumbnail in the specified folder
+        'outtmpl': 'static/thumbnail/%(title)s.%(ext)s',
+        'postprocessors': [{  # Post-processing step to convert to PNG
+            'key': 'ImageConvertor',
+            'format': 'png',  # Convert to PNG format
+        }]
+    }
+
+    with YoutubeDL(options) as ydl:
+        ydl.download([url])
+
+
+def get_video_info(url):
+    ydl_opts = {
+        'quiet': True,  # Suppress output
+    }
+
+    with YoutubeDL(ydl_opts) as ydl:
+        # Set download=False to prevent downloading
+        info_dict = ydl.extract_info(url, download=False)
+
+        info = {
+            "id": info_dict['id'],
+            "title": info_dict['title'],
+            "uploader": info_dict['uploader'],
+            "upload_date": info_dict['upload_date'],
+            "description": info_dict['description'],
+            "view_count": info_dict['view_count'],
+            "like_count": info_dict['like_count'],
+            "duration": info_dict['duration'],
+            "tags": info_dict['tags'],
+            "channel": info_dict['channel'],
+            "channel_url": info_dict['channel_url'],
+            "thumbnail": info_dict['thumbnail']
+        }
+        return info
