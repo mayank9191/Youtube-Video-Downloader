d93cbc342e2bc54b42dcbbdd7366467cb296909c
Mayank Kulahara <mayankkulahara@gmail.com>
2025-02-12 23:20:47 +0530

    merge code and optimize

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template,  redirect, request, url_for
+from Search import listvideo
 from Yt_Video_Downloader import download_yt_combined
+import fnmatch
 from urllib.parse import quote
 import os
-import fnmatch
 
 app = Flask(__name__)
 
@@ -14,28 +15,45 @@ def find_files_with_keyword(keyword, directory=r"C:\Users\Mayank\OneDrive\Deskto
             # Matches files with keyword anywhere
             if fnmatch.fnmatch(file, f"*{keyword}*"):
                 matching_files.append(os.path.join(root, file))
-    return matching_files.pop()
+    return matching_files[-1] if matching_files else None
 
 
-@app.route("/", methods=["POST", "GET"])
+@app.route("/", methods=["GET", "POST"])
 def home():
-
     if request.method == "POST":
-        url_fetched = request.form["video-url"]
 
-        name = download_yt_combined(url_fetched)
+        name = request.form["video_name"]
 
-        final_path = find_files_with_keyword(
-            keyword=name[0:8]).replace("\\", "/").split("DOWNLOADER/")[1]
+        video_list = listvideo(name)
 
-        # print(final_path)
-
-        encoded_filename = quote(final_path)
-        # print(encoded_filename)
-
-        return render_template("index.html", video=encoded_filename)
+        return render_template("index.html", list=video_list)
 
     return render_template("index.html")
+
+# In this video is being Served on this route
+
+
+@app.route("/<video_id>", methods=["GET", "POST"])
+def video(video_id):
+
+    url_fetched = f"https://www.youtube.com/watch?v={video_id}"
+
+    name = download_yt_combined(url_fetched)
+    # print(name)
+
+    if not name:
+        # Return an error message or handle the failure gracefully
+        return render_template("index.html", error="Video download failed, please try again.")
+
+    final_path = find_files_with_keyword(
+        keyword=name[0:8]).replace("\\", "/").split("DOWNLOADER/")[1]
+
+    # print(final_path)
+
+    encoded_filename = quote(final_path)
+    # print(encoded_filename)
+
+    return render_template("video.html", video=encoded_filename)
 
 
 if __name__ == "__main__":
